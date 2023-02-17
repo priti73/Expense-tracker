951279f72462e9078ef820bf960b17ebad8f4754
priti73 <pritikumarinits@gmail.com>
2023-02-17 19:35:38 +0530

    task3

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,28 @@
-const Expense = require('../models/users');
+const signup = require('../models/users');
+
+function validatestring(string){
+   if(string==undefined || string.length===0)
+    return true;
+    else {
+      return false;
+    }
+}
 
 exports.signup=async (req,res,next)=>{
-   const name=req.body.name;
-   const email=req.body.email;
-   const password=req.body.password;
+   
     try{
- 
-       if(!req.body.name || !req.body.email || !req.body.password){
-          throw new Error('Plz fill all the fields')
+       const {name,email,password }=req.body
+       if(validatestring(name) || validatestring(email) 
+       || validatestring(password)){
+         return res.status(400).json({error:"ALL feilds are required"})
        }
-   //   const name=req.body.name;
-   //   const email=req.body.email;
-   //   const password=req.body.password;
      
-      const data =await Expense.create({
+      const data =await signup.create({
          name:name,
          email:email,
          password: password
       });
-      console.log('new expense');
+      console.log('new user');
       res.status(201).json({newuser:data});
     }
     catch(err){
